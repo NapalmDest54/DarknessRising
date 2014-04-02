461145af24bbc35f8cd040bc4779942a7a69c8a4
NapalmDest54 <napalmdest54@gmail.com>
2014-04-02 19:44:47 -0400

    Updated spine  component to use meshes correctly.

--- DarknessRising/src/com/github/darknessrising/gameobjects/components/render/SpineComponent.java
+++ DarknessRising/src/com/github/darknessrising/gameobjects/components/render/SpineComponent.java
@@ -2,19 +2,26 @@ package com.github.darknessrising.gameobjects.components.render;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.esotericsoftware.spine.AnimationState;
+import com.esotericsoftware.spine.AnimationStateData;
 import com.esotericsoftware.spine.Skeleton;
 import com.esotericsoftware.spine.SkeletonData;
 import com.esotericsoftware.spine.SkeletonJson;
 import com.esotericsoftware.spine.SkeletonRenderer;
 import com.esotericsoftware.spine.SkeletonRendererDebug;
+import com.github.darknessrising.DarknessRisingGame;
 import com.github.darknessrising.gameobjects.GameObject;
 
 public class SpineComponent extends RenderComponent {
 	private Skeleton skeleton;
 	private SkeletonRenderer renderer;
 	private SkeletonRendererDebug debugRenderer;
-	
+	private PolygonSpriteBatch spriteBatchPoly = new PolygonSpriteBatch();
+	private AnimationState state;
+	Texture tex;
 	public SpineComponent(GameObject owner, String spinePath) {
 		super(owner);
 		renderer = new SkeletonRenderer();
@@ -24,21 +31,39 @@ public class SpineComponent extends RenderComponent {
 		SkeletonData skeletonData = json.readSkeletonData(Gdx.files.internal(spinePath + ".json"));
 
 		skeleton = new Skeleton(skeletonData); // Skeleton holds skeleton state (bone positions, slot attachments, etc).
-		skeleton.setX(100);
-		skeleton.setY(100);
+		skeleton.setX(10);
+		skeleton.setY(-100);
+		tex = new Texture(Gdx.files.internal("data/sprites/player.png"));
+		
+		AnimationStateData stateData = new AnimationStateData(skeletonData);
+		state = new AnimationState(stateData);
+		state.setAnimation(0, "animation", true);
+		
+		debugRenderer.setBones(true);
+		debugRenderer.setRegionAttachments(true);
 	}
 
 	@Override
 	public void render(OrthographicCamera camera) {
 		if (skeleton != null) {
-			//skeleton.updateWorldTransform();
-			System.out.println(skeleton.getX());
-			debugRenderer.getShapeRenderer().getProjectionMatrix().set(camera.combined);
-			spriteBatch.getProjectionMatrix().set(camera.combined);
-			spriteBatch.begin();
-			renderer.draw(spriteBatch, skeleton);
-			spriteBatch.end();
-			debugRenderer.draw(skeleton);
+			if (state != null) {
+				state.update(Gdx.graphics.getDeltaTime());
+				state.apply(skeleton);
+			}
+			
+			skeleton.updateWorldTransform();
+			spriteBatchPoly.getProjectionMatrix().set(camera.combined);
+			spriteBatchPoly.begin();
+			renderer.draw(spriteBatchPoly, skeleton);
+			spriteBatchPoly.end();
+			
+			
+			if (DarknessRisingGame.DEBUG) {
+				debugRenderer.getShapeRenderer().getProjectionMatrix().set(camera.combined);
+				debugRenderer.draw(skeleton);
+			}
+			
+			
 		}
 	}
 
