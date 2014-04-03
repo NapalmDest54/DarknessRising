dbba291d02cc9ca1ad826f31bfc67a008a8d2e0f
NapalmDest54 <napalmdest54@gmail.com>
2014-04-03 00:41:22 -0400

    Added physics component.

---
+++ DarknessRising/src/com/github/darknessrising/gameobjects/components/PhysicsComponent.java
@@ -0,0 +1,56 @@
+package com.github.darknessrising.gameobjects.components;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.github.darknessrising.gameobjects.GameObject;
+import com.github.evms.eventmangement.Event;
+import com.github.evms.eventmangement.EventHandler;
+import com.github.evms.eventmangement.EventManager;
+
+public class PhysicsComponent extends Component implements EventHandler {
+
+	private Body body;
+	private EventManager eventManager;
+	private float prevOrientation;
+	private Vector2 prevPosition;
+	
+	public static final float PIXELS_TO_METERS = 1/100f;
+	public static final float METERS_TO_PIXELS = 100f;
+	
+	public PhysicsComponent(GameObject owner, Body body) {
+		super(owner);
+		this.body = body;
+		eventManager = EventManager.getInstance();
+		prevPosition = new Vector2(body.getPosition());
+		prevOrientation = body.getAngle();
+		eventManager.raiseEvent(eventManager.getNewEvent("EVENT_GAMEOBJECT_PHYSICS_CHANGES", owner, body));
+		eventManager.registerEvent("EVENT_UPDATE_CALL", this);
+		eventManager.registerEvent("EVENT_APPLY_VELOCITY_CENTER", this);
+		body.setLinearVelocity(0, 0);
+		body.setLinearDamping(0.1f);
+		
+	}
+	
+	private void update() {
+		if (prevPosition.dst2(body.getPosition()) != 0 || prevOrientation != body.getAngle()) {
+			eventManager.raiseEvent(eventManager.getNewEvent("EVENT_GAMEOBJECT_PHYSICS_CHANGES", owner, body));
+		}
+	}
+	
+	@Override
+	public void onEvent(Event event) {
+		if (event.getType().equals("EVENT_UPDATE_CALL")) {
+			update();
+		} else if (event.getType().equals("EVENT_APPLY_VELOCITY_CENTER")) {
+			if (event.getParams()[0].equals(owner)) {
+				body.setLinearVelocity(((Vector2)event.getParams()[1]).x * 5, ((Vector2)event.getParams()[1]).y * 5);
+			}
+		}
+		
+	}
+
+	public Body getBody() {
+		return body;
+	}
+
+}
